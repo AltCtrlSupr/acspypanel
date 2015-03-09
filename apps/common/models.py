219d0191ad9401249c1074b0860a03d47c2b4cab
ZUNbado <git@acs.li>
2015-03-09 22:59:19 +0100

    adding django permissions

--- apps/common/models.py
+++ apps/common/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
 from django.contrib.contenttypes.models import ContentType
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Permission
 
 
 class ACSModelBase(models.Model):
@@ -25,3 +25,9 @@ class ACSPermission(models.Model):
     def __unicode__(self): return u'%s -> %s' % (self.content_type, self.content_object)
 
     def get_users(self): return u', '.join(user.username for user in self.user.all())
+
+    def save(self, *args, **kwargs):
+        super(ACSPermission, self).save(*args, **kwargs)
+        for user in self.user.all():
+            for p in Permission.objects.filter(content_type=ContentType.objects.get_for_model(self.content_object)):
+                user.user_permissions.add(p)
